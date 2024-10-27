3bb1f1182541f799118cbace3e9dd6f608bbe3db
Javier <javichav1.jp@gmail.com>
2024-10-27 19:57:35 +0100

    general
    Se separan los html para hacer redireccion mediante el .js. También se realiza el formulario de contacto usando html y css. Tdavía falta que envíe el correo u mensaje

---
+++ index.js
@@ -0,0 +1,11 @@
+function redirectAbout() {
+    window.location.href = "about.html"; 
+}
+
+function redirectHome() {
+    window.location.href = "index.html"; 
+}
+
+function redirectContact() {
+    window.location.href = "contact.html"; 
+}
